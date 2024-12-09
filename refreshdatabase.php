96c2f3151a5ecb51151b1e8eb3d7a0ffa90f218f
charles-webdev <charlesmichel37@protonmail.com>
2024-12-09 23:55:07 +0100

    header above connect

--- refreshdatabase.php
+++ refreshdatabase.php
@@ -1,14 +1,14 @@
 <?php
 
+$pageTitle = "Edit Game Details";
+require_once('header.php');
+
 # Get db connection
 connect();
 global $connection;
 
 $sql = file_get_contents(__DIR__."/videogame_db.sql");
 $query = runQuery($sql);
-
-$pageTitle = "Add game";
-require_once 'header.php';
 ?>
 
 <div>Database refreshed</div>
