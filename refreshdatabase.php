d28f5a38b7aa969bf81d07c492f9547651416c42
charles-webdev <charlesmichel37@protonmail.com>
2024-12-09 23:53:34 +0100

    remve includes on refreshDatabase

--- refreshdatabase.php
+++ refreshdatabase.php
@@ -1,10 +1,5 @@
 <?php
 
-// require_once("./includes/functions.inc.php");
-
-# declare required db
-require_once("./includes/database.inc.php");
-
 # Get db connection
 connect();
 global $connection;
