cf12dcafa64cc262676efbaef777cf1bcf4d7618
charles-webdev <charlesmichel37@protonmail.com>
2024-12-09 22:14:41 +0100

    add refreshbdd script

---
+++ refreshbdd.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("./includes/functions.inc.php");
+
+# declare required db
+require_once("./includes/database.inc.php");
+
+# Get db connection
+connect();
+global $connection;
+
+$sql = file_get_contents(__DIR__."/videogame_db.sql");
+$query = runQuery($sql);
