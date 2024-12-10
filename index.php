f99e2e2905d559e101d1555e06ecc08c3d6a7bc3
charles-webdev <charlesmichel37@protonmail.com>
2024-12-10 18:07:47 +0100

    Add tasks list to index

--- index.php
+++ index.php
@@ -9,6 +9,16 @@ include("header.php");
     <section class="welcome-section">
         <div class="container">
             <h2 class="welcome-text">Welcome to Game 'n Go!</h2>
+
+            <div class="feature-list">
+                <p>Tasks :</p>
+                <ul>
+                    <li>Log in as another user</li>
+                    <li>Fake admin user and add a game</li>
+                    <li>Inject script on another person's browser</li>
+                    <li>delete all comments in one action</li>
+                </ul>
+            </div>
         </div>
     </section>
 
